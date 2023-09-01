f134376b1fd7c9f8023ac5b627753575d8903127
rxzkie <54205705+rxzkie@users.noreply.github.com>
2023-08-31 21:11:29 -0400

    🥶🎪❄️ 💓☃️😝 state andando

--- asistencia_app/src/app/tab1/tab1.page.ts
+++ asistencia_app/src/app/tab1/tab1.page.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { StateService } from '../state.service'; // Ajusta la ruta según la ubicación real del servicio
 
-let navigationExtras: NavigationExtras = {
 
-};
+let navigationExtras: NavigationExtras = {};
 
 @Component({
   selector: 'app-tab1',
@@ -15,11 +15,19 @@ export class Tab1Page {
   username: string = '';
   password: string = '';
 
-  constructor(private router: Router, private navCtrl: NavController) {}
+  constructor(
+    private router: Router,
+    private navCtrl: NavController,
+    private stateService: StateService // Inyecta el servicio aquí
+  ) {}
 
   login() {
     // Verificar las credenciales (esto es solo un ejemplo)
+    
     if (this.username === 'admin' && this.password === 'hola') {
+      // Guardar el nombre de usuario en el servicio de estado
+      this.stateService.setUsername(this.username);
+
       this.router.navigate(['/tabs/tab2'], navigationExtras); // Redirigir a la Tab 2
       // O puedes usar navCtrl para redirigir:
       // this.navCtrl.navigateForward('/tabs/tab2');
@@ -31,5 +39,4 @@ export class Tab1Page {
   registrarse(){
     this.router.navigate(['/tabs/tab3']);
   }
-
 }
--- asistencia_app/src/app/tab2/tab2.page.ts
+++ asistencia_app/src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StateService } from '../state.service'; // Ajusta la ruta según la ubicación real del servicio
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  username: string = '';
 
-  constructor() {}
+  constructor(private stateService: StateService) {}
 
+  ngOnInit() {
+    // Obtén el nombre de usuario almacenado en el servicio y asígnalo a la variable
+    this.username = this.stateService.getUsername();
+  }
 }
