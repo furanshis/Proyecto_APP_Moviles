4cf4427068be0bcb5dc794935385c7dc6eb64a1a
Francisco Santander <71709143+furanshis@users.noreply.github.com>
2023-09-26 10:50:27 -0300

    hb

---
+++ asistencia_app/src/app/apirest.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ApiService } from './apirest.service';
+
+describe('ApirestService', () => {
+  let service: ApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ApiService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
