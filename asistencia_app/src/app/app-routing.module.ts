88211655b368ab10f4371e409e4e9d4604d13186
Furanshis <71709143+furanshis@users.noreply.github.com>
2023-11-01 18:21:23 -0300

    cosas

--- asistencia_app/src/app/app-routing.module.ts
+++ asistencia_app/src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { NoAuthGuard } from './guards/no-auth.guard';
-import { AuthGuard } from './guards/auth.guard';
+//import { NoAuthGuard } from './guards/no-auth.guard';
+//import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule), canActivate:[NoAuthGuard]
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)//, canActivate:[NoAuthGuard]
   },
   {
     path: 'alumnos',
-    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule), canActivate:[AuthGuard]
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)//, canActivate:[AuthGuard]
   },
   {
     path: 'contrasena',
