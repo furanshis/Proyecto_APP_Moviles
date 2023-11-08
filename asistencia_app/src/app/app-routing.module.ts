a78c3b215392b614659428e48a7f33760bd2e399
Furanshis <71709143+furanshis@users.noreply.github.com>
2023-11-07 22:50:16 -0300

    guards funcionando

--- asistencia_app/src/app/app-routing.module.ts
+++ asistencia_app/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 //import { NoAuthGuard } from './guards/no-auth.guard';
 //import { AuthGuard } from './guards/auth.guard';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard'
+import { Tab1Page } from './tab1/tab1.page';
 
 const routes: Routes = [
   {
@@ -10,15 +12,23 @@ const routes: Routes = [
   },
   {
     path: 'alumnos',
-    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)//, canActivate:[AuthGuard]
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule), 
+    ...canActivate(() => redirectUnauthorizedTo(['/tabs/tab1/']))
   },
   {
     path: 'contrasena',
-    loadChildren: () => import('./contrasena/contrasena.module').then( m => m.ContrasenaPageModule)
+    loadChildren: () => import('./contrasena/contrasena.module').then( m => m.ContrasenaPageModule),
+    ...canActivate(() => redirectLoggedInTo(['/tabs/tab2']))
   },
   {
     path: 'codigo',
-    loadChildren: () => import('./codigo/codigo.module').then( m => m.CodigoPageModule)
+    loadChildren: () => import('./codigo/codigo.module').then( m => m.CodigoPageModule),
+    ...canActivate(() => redirectLoggedInTo(['/tabs/tab2']))
+  },
+  {
+    path: 'tabs/tab1',
+    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule),
+    ...canActivate(() => redirectLoggedInTo(['/tabs/tab2']))
   }
 
 ];
